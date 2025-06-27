d63009374047767dcbf6f7fb0cd3e87a97d33772
Schubert Anselme <schubert@anselm.es>
2025-06-27 12:30:19 -0400

    devcontainer: Update Dockerfile and Docker Compose configuration for development environment
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

---
+++ scripts/configure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2025 Schubert Anselme <schubert@anselm.es>
+# SPDX-Licence-Identifier: GPL-3.0
+
+set -exo pipefail
+
+DIR="$(dirname $(realpath $(dirname "${0}")))"
+
+# configure environment
+if [[ ! -d "/home/ubuntu/.oh-my-zsh" ]]; then
+  curl -fsSLo /tmp/ohmyzsh-install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+  bash /tmp/ohmyzsh-install.sh --unattended || true
+  rm -f /tmp/ohmyzsh-install.sh
+fi
+
+sudo ln -sf "${DIR}/scripts/aliases.sh" /etc/profile.d/aliases.sh
+sudo ln -sf "${DIR}/scripts/environment.sh" /etc/profile.d/environment.sh
